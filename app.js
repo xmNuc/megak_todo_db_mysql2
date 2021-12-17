c66bc7ad98923630749e7a31f46534270069cf92
xmNuc <xmnuc@o2.pl>
2021-12-17 02:43:42 +0100

    3rd Commit

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const { TodoRecord } = require('./records/todo.record');
 const { pool } = require('./utils/db');
 
 (async () => {
-  const foundTodo = await TodoRecord.find(
-    'cecf2aa3-9bee-46c5-b6f8-c3644025d5aa'
-  );
-  foundTodo.title = 'Buy me more cocaine';
-  console.log(foundTodo);
-  await foundTodo.update();
+  const findAll = await TodoRecord.findAll();
+  console.log(findAll);
+
+  //   const foundTodo = await TodoRecord.find(
+  //     'cecf2aa3-9bee-46c5-b6f8-c3644025d5aa'
+  //   );
+  //   foundTodo.title = 'Buy me more cocaine XD';
+  //   console.log(foundTodo);
+  //   await foundTodo.update();
 
   //   const foundTodo = await TodoRecord.find(
   //     'd2bed4a5-6ca9-48f1-9b10-474ae89bc36f'
