458186198abb7b14d3da852699df907807bf7718
xmNuc <xmnuc@o2.pl>
2021-12-21 00:09:24 +0100

    3th Commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const { TodoRecord } = require('./records/todo.record');
+const { TodoRepository } = require('./repositories/todo.repository');
 const { pool } = require('./utils/db');
 
 (async () => {
-  const findAll = await TodoRecord.findAll();
-  console.log(findAll);
+  const foundTodo = await TodoRepository.find(
+    '1112d407-f85c-41e2-885b-e903b8f3b591'
+  );
+  foundTodo.title = 'Sialalala 2';
+  await TodoRepository.update(foundTodo);
+  console.log(foundTodo);
+  console.log(await TodoRepository.findAll());
+
+  // const findAll = await TodoRepository.findAll();
+  // console.log(findAll);
+
+  // const findAll = await TodoRecord.findAll();
+  // console.log(findAll);
 
   //   const foundTodo = await TodoRecord.find(
   //     'cecf2aa3-9bee-46c5-b6f8-c3644025d5aa'
